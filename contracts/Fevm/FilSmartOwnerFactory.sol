2c19edc86f952c023b713aa6285321618312cbfd
Ashton Chen <cdljsj@gmail.com>
2023-04-03 16:16:34 +0800

    fix: no way to get smart owner address

--- contracts/Fevm/FilSmartOwnerFactory.sol
+++ contracts/Fevm/FilSmartOwnerFactory.sol
@@ -7,9 +7,15 @@ import "../Interfaces/ISmartAccountFactory.sol";
 contract FilSmartOwnerFactory is ISmartAccountFactory {
     mapping(address => bool) public smartOwnerPool;
 
-    function createSmartOwner(address governor) external {
+    event SmartOwnerCreated(address creator, address newSmartOwner);
+
+    function createSmartOwner(address governor) external returns(address) {
         MinerSmartOwner newSmartOwner = new MinerSmartOwner(governor);
         smartOwnerPool[address(newSmartOwner)] = true;
+
+        emit SmartOwnerCreated(address(msg.sender), address(newSmartOwner));
+
+        return address(newSmartOwner);
     }
 
     function isValidSmartAccount(address smartOwner) external view returns (bool) {
